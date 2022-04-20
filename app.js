1d65e972dc92fcc0c964c1ccdc41b7da655e457d
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-04-20 15:09:04 +0000

    Creation de la partie boutique

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(()=>{
+	$('.slide').slick({
+  	    dots: false,
+    	infinite: true,
+    	speed: 300,
+    	cssEase: 'ease-in',
+    	nextArrow: $('.next'),
+    	prevArrow: $('.prev')
+});
+		
+})
