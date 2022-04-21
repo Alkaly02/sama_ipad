fee8ff663ba4b2f30643a1e4e66d204687e42485
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-04-21 15:27:57 +0000

    Mise en place de la barre de navigation

--- app.js
+++ app.js
@@ -16,6 +16,37 @@ $(document).ready(()=>{
     	nextArrow: $('.nextTemoin'),
     	prevArrow: $('.prevTemoin')
 	});
+
+	$('.toggle-btn').click( () => {
+		$('#menu').stop().slideToggle();
+	})
+
+	$('.nav__list--link').click( () => {
+		$('#menu').slideToggle();
+	})
+
+	// Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 		
 })
 
@@ -23,5 +54,4 @@ const allImages = document.querySelectorAll('img');
 
 allImages.forEach( image => {
 	image.setAttribute('loading', 'lazy');
-	console.log(image)
 })
